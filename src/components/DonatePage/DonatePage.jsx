06f77246a28cb65de8f86e62e490a6e3af3d659b
Latisha Copeland <lcopeland001@gmail.com>
2023-05-18 13:43:14 -0500

    sagas and reducers on donate page

--- src/components/DonatePage/DonatePage.jsx
+++ src/components/DonatePage/DonatePage.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
-import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,9 +9,19 @@ import { useSelector } from 'react-redux';
 // or even care what the redux state is'
 
 function DonatePage() {
-  const donerDetails = useSelector(store => store.donerDetails);
+  const donerDetails = useSelector(store => store.donerReducer);
+  const [donerFirstName, setDonerFirstName] = useState('');
   
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchDonerDetails();
+  }, []);
+
+  const fetchDonerDetails = () => {
+    dispatch({ type: 'FETCH_DONER_DETAILS'});
+  };
+
   return (
     <div className="container">
       <div>
